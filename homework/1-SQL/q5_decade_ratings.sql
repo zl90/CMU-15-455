5c324ce279d6a4ae778361a063d073ab53498da7
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-13 22:19:22 +1000

    Add comment

--- homework/1-SQL/q5_decade_ratings.sql
+++ homework/1-SQL/q5_decade_ratings.sql
@@ -1,3 +1,7 @@
+/*  Compute intersting statistics on the ratings of content on a per-decade basis.
+ 
+ Details: Get the average rating (rounded to two decimal places), top rating, min rating, and the number of releases in each decade. Exclude titles which have not been premiered (i.e. where premiered is NULL). Print the relevant decade in a fancier format by constructing a string that looks like this: 1990s. Order the decades first by their average rating in a descending fashion and secondly by the decade, ascending, to break ties.
+ Your output should have the format: DECADE|AVG_RATING|TOP_RATING|MIN_RATING|NUM_RELEASES  */
 SELECT CAST(FLOOR(titles.premiered / 10) * 10 AS STRING) || 's' AS decade,
     ROUND(AVG(ratings.rating), 2) AS avg_rating,
     MAX(ratings.rating) AS max_rating,
