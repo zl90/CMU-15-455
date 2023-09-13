73bc0e6c94f7841b1ca88ecfd198a226a326f87b
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-13 22:18:34 +1000

    Complete question 5

--- homework/1-SQL/q5_decade_ratings.sql
+++ homework/1-SQL/q5_decade_ratings.sql
@@ -1,6 +1,12 @@
 SELECT CAST(FLOOR(titles.premiered / 10) * 10 AS STRING) || 's' AS decade,
-    AVG(ratings.rating) AS rating
+    ROUND(AVG(ratings.rating), 2) AS avg_rating,
+    MAX(ratings.rating) AS max_rating,
+    MIN(ratings.rating) AS min_rating,
+    COUNT(DISTINCT(titles.primary_title)) AS title
 FROM ratings
     JOIN titles ON ratings.title_id = titles.title_id
+WHERE titles.premiered IS NOT NULL
 GROUP BY decade
+ORDER BY avg_rating DESC,
+    decade ASC
 LIMIT 20;
